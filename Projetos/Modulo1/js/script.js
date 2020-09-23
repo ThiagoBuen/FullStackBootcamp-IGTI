e547a6bac471648404a93a4d96b749e1c2c16fa5
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-09-23 11:36:08 -0300

    23/09 - Começo do projeto pratico 1

---
+++ Projetos/Modulo1/js/script.js
@@ -0,0 +1,8 @@
+// 3 textos
+// 3 inputs do tipo range
+// 3 inputs de text desabilitados
+
+// Monitorar o on change dos input range, refletir nos inputs de texto e trocar a cor do div
+// Delimitar no html os limites min e max do range no html
+// Background color, função RGB do CSS para aceitar os 3 valores
+// Passar uma string via JS para o CSS
