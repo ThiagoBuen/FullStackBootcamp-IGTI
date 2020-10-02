b407d5b95293d6951fd1d54f0dd9b3df487b98be
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-10-01 22:47:28 -0300

    Desafio do modulo 2 feito

--- Projetos/Modulo2/js/script.js
+++ Projetos/Modulo2/js/script.js
@@ -10,11 +10,13 @@ let averageAve = 0;
 
 window.addEventListener('load', () => {
   filteredUserMessage = document.querySelector('#userCountMessage');
-  tabFoundUsers = document.querySelector('#tabFoundUsers');
+  tabFilteredUsers = document.querySelector('#tabFoundUsers');
   totalMales = document.querySelector('#totalMaleCount');
   totalFemales = document.querySelector('#totalFemaleCount');
   totalAge = document.querySelector('#totalAge');
   ageAverage = document.querySelector('#ageAverage');
+  inputNameSearch = document.querySelector('#inputNameSearch');
+  submitFilter = document.querySelector('#submitFilter');
 
   fetchUsers();
 });
@@ -26,13 +28,110 @@ async function fetchUsers() {
   const json = await res.json();
   console.log(json);
   //prettier-ignore
-  allUsers = json.map((user) => {
+  allUsers = json.results.map((user) => {
+    
     return {
       name: user.name.first + ' ' + user.name.last,
       gender: user.gender,
       picture: user.picture.medium,
-      age: user.dob.age,
+      age: user.dob.age
     }
   });
   console.log(allUsers);
+  inputNameSearch.addEventListener('keyup', enableSubmit);
+  submitFilter.addEventListener('click', filterUsers);
+}
+
+function enableSubmit(event) {
+  if (event.target.value === '') {
+    submitFilter.disabled = true;
+  } else {
+    submitFilter.disabled = false;
+    if (event.key === 'Enter') {
+      //console.log('Foi enter!');
+      filterUsers();
+    }
+  }
+  console.log(event);
+}
+
+function filterUsers() {
+  let inputName = inputNameSearch.value;
+  //console.log(inputName);
+  //console.log(allUsers.forEach((user) => console.log(user.name)));
+  filteredUsers = allUsers.filter((user) => user.name.includes(inputName));
+  console.log(filteredUsers);
+  render();
+}
+
+function render() {
+  filterRender();
+  updateFilteredUsersNumberField();
+  updateStatisticsNumberFields();
+}
+
+function filterRender() {
+  let filterUsersHTML = '<div>';
+  filteredUsers.forEach((user) => {
+    const { name, gender, picture, age } = user;
+    const filterUserHTML = `
+      <div class='user'>
+        <div>
+          <img src="${picture}" >
+        </div>
+        <div>
+          ${name} , ${age} anos
+        </div>
+    `;
+    filterUsersHTML += filterUserHTML;
+  });
+  filterUsersHTML += '</div>';
+  tabFilteredUsers.innerHTML = filterUsersHTML;
+}
+
+function updateFilteredUsersNumberField() {
+  let numFilterUsers = filteredUsers.length;
+
+  if (numFilterUsers > 0) {
+    filteredUserMessage.textContent =
+      numFilterUsers + ' usuário(s) filtrado(s)';
+  } else {
+    filteredUserMessage.textContent = 'Nenhum usuário filtrado';
+  }
+}
+
+function updateStatisticsNumberFields() {
+  let totalFoundMales = 0;
+  filteredUsers.forEach((user) => {
+    if (user.gender === 'male') {
+      totalFoundMales += 1;
+    }
+  });
+
+  let totalFoundFemales = 0;
+  filteredUsers.forEach((user) => {
+    if (user.gender === 'female') {
+      totalFoundFemales += 1;
+    }
+  });
+
+  const totalSumAge = filteredUsers.reduce((accumulator, current) => {
+    return (accumulator += current.age);
+  }, 0);
+
+  averageAve = totalSumAge / filteredUsers.length;
+
+  totalMales.textContent = totalFoundMales;
+  totalFemales.textContent = totalFoundFemales;
+  totalAge.textContent = totalSumAge;
+  ageAverage.textContent = averageAve;
+
+  console.log('Total Found Males ' + totalFoundMales);
+  console.log('Total Found Females ' + totalFoundFemales);
+  console.log('TOtal aGE: ' + totalSumAge);
+  console.log('Average aGE: ' + averageAve);
+  /*let totalMales = 0;
+let totalFemale = 0;
+let totalAge = 0;
+let averageAve = 0;*/
 }
