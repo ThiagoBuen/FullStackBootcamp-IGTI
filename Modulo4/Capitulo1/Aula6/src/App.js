adcd5ffae4c03ebb7eb8d44345d45671bb055162
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-10-21 21:51:06 -0300

    21/10 - Desafio 2 going

--- Modulo4/Capitulo1/Aula6/src/App.js
+++ Modulo4/Capitulo1/Aula6/src/App.js
@@ -8,6 +8,8 @@ export default class App extends Component {
 
     this.state = {
       allCountries: [],
+      filteredCountries: [],
+      filter: '',
     };
   }
 
@@ -26,15 +28,22 @@ export default class App extends Component {
 
     this.setState({
       allCountries,
+      filteredCountries: allCountries,
     });
   }
+
+  handleChangeFilter = (newText) => {
+    this.setState({
+      filter: newText,
+    });
+  };
   render() {
-    const { allCountries } = this.state;
+    const { allCountries, filter } = this.state;
 
     return (
       <div className="container">
         <h1>React Countries</h1>
-        <Header />
+        <Header filter={filter} onChangeFilter={this.handleChangeFilter} />
 
         <Countries countries={allCountries} />
       </div>
--- Modulo4/Capitulo1/Aula6/src/components/header/Header.js
+++ Modulo4/Capitulo1/Aula6/src/components/header/Header.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
+  handleInputChange = (event) => {
+    const newText = event.target.value;
+    console.log(event.target.value);
+    this.props.onChangeFilter(newText);
+  };
   render() {
+    const { filter } = this.props;
     return (
       <div>
-        <input type="text" /> | <span>Países: </span> |<span> População: </span>
+        <input type="text" value={filter} onChange={this.handleInputChange} /> |{' '}
+        <span>Países: </span> |<span> População: </span>
       </div>
     );
   }
