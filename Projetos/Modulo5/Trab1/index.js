a34c583c325e152f3409c78af9d222df700a6578
ThiagoBuen <thiagobueno@gea.inatel.br>
2020-11-08 17:43:47 -0300

    08/11 - dotenv

--- Projetos/Modulo5/Trab1/index.js
+++ Projetos/Modulo5/Trab1/index.js
@@ -2,14 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { accountRouter } from './routes/accountsRouter.js';
 
+require('dotenv').config;
 const url =
-  'mongodb+srv://<db>:<password>@cluster0.i0ege.mongodb.net/<db>?retryWrites=true&w=majority';
+  'mongodb+srv://{$process.env.USERDB}:<{$process.env.PWDDB}>@cluster0.i0ege.mongodb.net/<db>?retryWrites=true&w=majority';
 
 const app = express();
 app.use(express.json());
 app.use(accountRouter);
 
-app.listen(3001, () => console.log('API iniciada'));
+app.listen(process.env.PORT, () => console.log('API iniciada'));
 
 const connect = async () => {
   try {
